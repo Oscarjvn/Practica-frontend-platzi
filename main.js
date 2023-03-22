25750bb2fe7aac8d7ff36cae1bcf591bba0bf992
Oscar Vilchez <oscarjosevilchez2@gmail.com>
2023-03-21 21:47:33 -0400

    agregando los productos al documento a traves de javascript

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const cartButton = document.querySelector(".cart-icon");
 const asideOrders = document.querySelector(".aside-container")
 
+//variables para los productos
+const productCardsContainer = document.querySelector(".cards-container");
+
+
 
 
 console.log(desktopMenuEmail, desktopMenu)
@@ -39,3 +43,80 @@ function toggleAsideCart(){
 desktopMenuEmail.addEventListener("click", toggleDesktopMenu)
 botonMenuMobile.addEventListener("click", toggleMobilepMenu)
 cartButton.addEventListener("click", toggleAsideCart)
+
+//logica para agregar productos a traves de un array al html simulando el consumo de un apirest
+
+let productList = [];
+productList.push({
+    name: "Honda Nsx",
+    price: 30000,
+    image: "https://rare-gallery.com/mocahbig/85393-acura-nsx-honda-cars-hd-4k.jpg",
+})
+
+productList.push({
+    name: "Mazda RX7",
+    price: 25000,
+    image: "https://www.fastcar.co.uk/wp-content/uploads/sites/2/Rocket-Bunny-FC-RX-7-14.jpg?w=900",
+})
+
+productList.push({
+    name: "Nissan R32",
+    price: 45000,
+    image: "https://cdn1.mecum.com/auctions/ca0822/ca0822-532316/images/1-1658871749103@2x.jpg?1660951736000",
+})
+
+productList.push({
+    name: "alto gato",
+    price: 45000,
+    image: "C:/Users/oscar y chuck/Desktop/54299ab7c771c5c563f7179d3f16aab1.jpg",
+})
+
+
+
+/*             <div class="product-container">
+                <img src="https://rare-gallery.com/mocahbig/85393-acura-nsx-honda-cars-hd-4k.jpg" alt="nsx">*
+        
+                <div class="princing-container">
+                    <div class="product-info">
+                        <p>50,000$</p>
+                        <p>Honda nsx</p>
+                    </div>
+                    <figure>
+                        <img src="./Platzi_YardSale_Icons/bt_add_to_cart.svg" alt="cart">
+                    </figure>
+                </div>
+            </div> */
+
+for(let product of productList){
+   
+    let productContainer = document.createElement("div");
+    productContainer.classList.add("product-container")
+
+    let productImg = document.createElement("img")
+    productImg.setAttribute("src", product.image)
+    let princingContainer = document.createElement("div")
+    princingContainer.classList.add("princing-container")
+    
+    productContainer.append(productImg, princingContainer)
+
+    let productInfo = document.createElement("div");
+    productInfo.classList.add("product-info")
+
+    let p1 = document.createElement("p")
+    p1.textContent = product.price
+    let p2 = document.createElement("p")
+    p2.textContent = product.name
+
+    productInfo.append(p1, p2)
+
+    let figure = document.createElement("figure")
+    let cartIcon = document.createElement("img")
+    cartIcon.setAttribute("src", "./Platzi_YardSale_Icons/bt_add_to_cart.svg")
+    figure.appendChild(cartIcon)
+
+    princingContainer.append(productInfo, figure)
+
+    productCardsContainer.appendChild(productContainer)
+
+}            
+console.log(productList)
